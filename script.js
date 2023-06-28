af23a7a947fd817614e99b18e7d843ba5717f72c
Yunzhi Li <melissaliyunzhi@gmail.com>
2023-06-28 22:53:50 +0100

    Update search function

---
+++ script.js
@@ -0,0 +1,14 @@
+// Get the search form and search input element
+const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('input[type="search"]');
+
+// Add an event listener to the form submission
+searchForm.addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevent the form from submitting
+
+  // Get the search query from the input field
+  const query = searchInput.value;
+
+  // Redirect to the search results page with the query as a URL parameter
+  window.location.href = `search-results.html?search-query=${query}`;
+});
