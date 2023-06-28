0e75a93daa1eea470f04a2b424246385e872c9a3
Yunzhi Li <melissaliyunzhi@gmail.com>
2023-06-28 23:38:26 +0100

    update search function

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-// Get the search form and search input element
-const searchForm = document.querySelector('form');
-const searchInput = document.querySelector('input[name="search-query"]');
-
-// Add an event listener to the form submission
-searchForm.addEventListener('submit', function(e) {
-    event.preventDefault(); // Prevent the form from submitting
-
-  // Get the search query from the input field
-  const query = searchInput.value;
-
-  // Redirect to the search results page with the query as a URL parameter
-  window.location.href = 'search-results.html?search-query=' + encodeURIComponent(searchInput.value);
-});
+document.addEventListener('DOMContentLoaded', function() {
+    // Retrieve the search query from the URL parameter
+    var searchQuery = decodeURIComponent(getParameterByName('search-query'));
+  
+    // Display the search query
+    var searchQueryElement = document.createElement('p');
+    searchQueryElement.textContent = 'Search Query: ' + searchQuery;
+    document.getElementById('searchResults').appendChild(searchQueryElement);
+  
+    // Filter and display the matching products
+    var products = document.querySelectorAll('.card');
+    products.forEach(function(product) {
+      var productName = product.querySelector('.card-title').textContent.toLowerCase();
+      if (productName.includes(searchQuery)) {
+        var clone = product.cloneNode(true);
+        clone.style.display = 'block';
+        document.getElementById('searchResults').appendChild(clone);
+      }
+    });
+  });
+  
+  // Helper function to retrieve URL parameters
+  function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  }
+  
