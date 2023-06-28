70dca641a9d90b32bc2a1d8f480861726722c179
Yunzhi Li <melissaliyunzhi@gmail.com>
2023-06-28 23:14:56 +0100

    update search script

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const searchForm = document.querySelector('form');
 const searchInput = document.querySelector('input[name="search-query"]');
 
 // Add an event listener to the form submission
-searchForm.addEventListener('submit', function (event) {
-  event.preventDefault(); // Prevent the form from submitting
+searchForm.addEventListener('submit', function(e) {
+    event.preventDefault(); // Prevent the form from submitting
 
   // Get the search query from the input field
   const query = searchInput.value;
 
   // Redirect to the search results page with the query as a URL parameter
-  window.location.href = `search-results.html?search-query=${query}`;
+  window.location.href = 'search-results.html?search-query=' + encodeURIComponent(searchInput.value);
 });
