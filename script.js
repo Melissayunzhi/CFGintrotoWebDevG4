21a43e2b80b77ef38908eb64fbbfa44b484cbc9d
Yunzhi Li <melissaliyunzhi@gmail.com>
2023-06-29 15:15:44 +0100

    search function

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const searchForm = document.querySelector('form');
-    const searchInput = document.querySelector('input[type="search"]');
-    
+    const searchForm = document.getElementById('searchForm');
+    const searchInput = document.getElementById('search-query');
+  
     // Retrieve the search query from the URL parameter
     var searchQuery = decodeURIComponent(getParameterByName('search-query'));
-    
+  
     // Display the search query
     var searchQueryElement = document.createElement('p');
     searchQueryElement.textContent = 'Search Query: ' + searchQuery;
     document.getElementById('searchResults').appendChild(searchQueryElement);
-    
+  
     // Filter and display the matching products
     var products = document.querySelectorAll('.card');
   
     searchForm.addEventListener('submit', function(event) {
       event.preventDefault();
-    
-      const query = searchInput.value;
-      window.location.href = `search-results.html?search-query=${query}`;
-      
+  
+      const query = searchInput.value.toLowerCase();
+      window.location.href = `search-results.html?search-query=${encodeURIComponent(query)}`;
+  
       products.forEach(function(product) {
         var productName = product.querySelector('.card-title').textContent.toLowerCase();
-        if (productName.includes(searchQuery)) {
+        if (productName.includes(query)) {
           product.style.display = 'block';
         } else {
           product.style.display = 'none';
         }
       });
     });
-    
+  
     // Helper function to retrieve URL parameters
     function getParameterByName(name, url) {
       if (!url) url = window.location.href;
